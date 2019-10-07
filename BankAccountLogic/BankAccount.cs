3cb9f02e2b7332c82767184158c3d0bfcc0c9616
Dennis Kafton <boom1axe@att.net>
2019-10-07 15:47:43 -0700

    Created Multi-project

---
+++ BankAccountLogic/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BankAccountBusinessLogic
+{
+    public class BankAccount
+    {
+
+
+        /// <summary>
+        /// Creates a bank account with given
+        /// account number. The account number
+        /// can be any alphanumeric string.
+        /// </summary>
+        /// <param name="accNum"></param>
+        public BankAccount(string accNum)
+        {
+            this.AccountNumber = accNum;
+        }
+
+        public string AccountNumber { get; private set; }
+
+        public double Balance { get; private set; }
+        
+        /// <summary>
+        /// Adds a given amount to the balance. returns
+        /// the new balance.
+        /// </summary>
+        /// <param name="amt">The positive amount to deposit</param>
+        /// <returns></returns>
+        public double Deposit (double amt)
+        {
+            if (amt <= 0)
+            {
+                throw new ArgumentException($"{nameof(amt)} must be positive!");
+            }
+
+            Balance += amt;
+            return Balance;
+        }
+
+        /// <summary>
+        /// Withdraws a gigven amount from the balance.
+        /// Returns the balance minus the amount withdrawn
+        /// </summary>
+        /// <param name="amt"></param>
+        /// <returns></returns>
+        public double Withdraw(double amt)
+        {
+            Balance -= amt;
+            return Balance;
+        }
+    }
+}
--- MultiProjectBankAccount/Program.cs
+++ MultiProjectBankAccount/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using BankAccountBusinessLogic;
+using System;
+using static System.Console;
 
 namespace MultiProjectBankAccount
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Welcome to Robbers Bank!");
+            WriteLine("Please enter an account number: ");
+
+            string accNum = ReadLine();
+            BankAccount acc = new BankAccount(accNum);
+
+            WriteLine("\n \n Enter an amount to deposit:");
+            double amount = Convert.ToDouble(ReadLine());
+
+            acc.Deposit(amount);
+            WriteLine($"the new balance is: {acc.Balance}");
+            ReadKey();
         }
     }
 }

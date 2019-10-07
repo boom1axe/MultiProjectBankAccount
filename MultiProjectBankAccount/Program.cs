a373ca871b61db947ca6c84bd8de6491f67f5e0b
Dennis Kafton <boom1axe@att.net>
2019-10-07 13:33:49 -0700

    Add project files.

---
+++ MultiProjectBankAccount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiProjectBankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
